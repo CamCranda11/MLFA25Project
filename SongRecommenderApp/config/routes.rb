088900489759deb5cf0158a25bdd6139191a8c8e
CamCranda11 <159499223+CamCranda11@users.noreply.github.com>
2025-11-29 09:39:21 -0700

    Commenting code.

--- SongRecommenderApp/config/routes.rb
+++ SongRecommenderApp/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  # Set the root URL to the search action.
   root 'recommendations#search'
+  # Define a GET route for the search.
   get 'recommendations/search'
 end
